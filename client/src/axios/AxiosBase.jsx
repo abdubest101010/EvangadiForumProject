9718474aac3b2cc97ea2d7db361eb695bb9024aa
abdubest101010 <101792283+abdubest101010@users.noreply.github.com>
2024-07-26 09:44:34 +0300

    final

--- client/src/axios/AxiosBase.jsx
+++ client/src/axios/AxiosBase.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosBase = axios.create({
-  baseURL: 'http://localhost:52896/api'
+  // baseURL: 'http://localhost:52896/api',
+  baseURL: "https://evangadiforumproject-nmdr.onrender.com",
   // baseURL: "https://evangadiforumbackend-8vl4.onrender.com/api",
   // baseURL:"https://evangadiforumbackend-1-zkgl.onrender.com/api"
 });
